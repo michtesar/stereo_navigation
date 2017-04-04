1d737bc9a1e0261aa13932d0766144914e9c6a0d
Michael Tesař <michael.tesar@nudz.cz>
2017-04-04 08:48:00 +0200

    Color management

--- SceneDemo.m
+++ SceneDemo.m
@@ -1,5 +1,4 @@
 function SceneDemo
-
 AssertOpenGL;
 screenid=max(Screen('Screens'));
 InitializeMatlabOpenGL(1);
@@ -10,50 +9,29 @@ Screen('BeginOpenGL', win);
 
 lighting();
 
-glMatrixMode(GL_PROJECTION);
+glMatrixMode(GL.PROJECTION);
 glLoadIdentity;
 gluPerspective(25, 1/ar, 0.1, 100);
 glMatrixMode(GL_MODELVIEW);
 glLoadIdentity;
-gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+gluLookAt(0, 4, -10, 0, 0, 0, 0, 1, 0);
 glClearColor(0, 0, 0, 0);
 glPushMatrix;
 
 glClear;
 
-readtexture('ball_01.jpg');
+drawsphere(1, -2.5, 0, 2, [1, 0, 0, 0]);
+drawsphere(1, 3.5, 0, -3, [0, 0, 1, 0]);
 
-color = [1.0, 0.0, 0.0, 1.0];
-glMaterialfv(GL_FRONT, GL_DIFFUSE, color);
-glTranslatef(-1.5,0,0);
-qobj = gluNewQuadric();
-gluQuadricTexture(qobj, GL_TRUE);
-glEnable(GL_TEXTURE_2D);
-gluSphere(qobj, 1, 80, 80);
-glDisable(GL_TEXTURE_2D);
-
-readtexture('ball_03.jpg');
-
-color = [1.0, 0.0, 0.0, 1.0];
-glMaterialfv(GL_FRONT, GL_DIFFUSE, color);
-glTranslatef(3.5,0,-3);
-qobj = gluNewQuadric();
-gluQuadricTexture(qobj, GL_TRUE);
-glEnable(GL_TEXTURE_2D);
-gluSphere(qobj, 1, 80, 80);
-glDisable(GL_TEXTURE_2D);
+drawtorus(2, 10, -2, 0, -2, 'ball_04.jpg', 100);
 
 glPopMatrix;
 
 Screen('EndOpenGL', win);
 Screen('Flip', win);
-Screen('BeginOpenGL', win);
 
 KbWait;
-
-Screen('EndOpenGL', win);
 sca;
-
 end
 
 function readtexture(source)
@@ -69,7 +47,7 @@ glTexParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT);
 glTexParameterf(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT);
 glTexParameterf(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
 glTexParameterf(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
-glTexEnvf(GL.TEXTURE_ENV, GL.TEXTURE_ENV_MODE, GL.DECAL);
+glTexEnvf(GL.TEXTURE_ENV, GL.TEXTURE_ENV_MODE, GL.COMBINE);
 glTexImage2D(GL.TEXTURE_2D, 0, GL.RGB, img_size(1), img_size(2),...
     0, GL.RGB, GL.UNSIGNED_BYTE, img_data);
 end
@@ -88,7 +66,27 @@ glLightf(GL.LIGHT0, GL.CONSTANT_ATTENUATION, 0.1);
 glLightf(GL.LIGHT0, GL.LINEAR_ATTENUATION, 0.05);
 end
 
-function drawsphere(size, x, y, z, texture)
+function drawsphere(size, x, y, z, color)
 global GL
+readtexture('ball_01.jpg');
+glMaterialfv(GL.FRONT, GL.DIFFUSE, color);
+glTranslatef(x, y, z);
+qobj = gluNewQuadric();
+gluQuadricTexture(qobj, GL.TRUE);
+glEnable(GL.TEXTURE_2D);
+gluSphere(qobj, size, 80, 80);
+glDisable(GL.TEXTURE_2D);
+end
 
+function drawtorus(inner, outter, x, y, z, texture, angle)
+global GL
+readtexture(texture);
+glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
+glTranslatef(x, y, z);
+glRotatef(angle, 1, 0, 0);
+qobj = gluNewQuadric();
+gluQuadricTexture(qobj, GL.TRUE);
+glEnable(GL.TEXTURE_2D);
+glutSolidTorus(inner, outter, 30, 30);
+glDisable(GL.TEXTURE_2D);
 end
