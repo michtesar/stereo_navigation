7ed91ce8569862e8b82222a4a7df34e5010811e2
neuropacabra <michael.tesar@nudz.cz>
2017-07-03 23:42:49 +0200

    Testing scale ready

--- arenaAngles.m
+++ arenaAngles.m
@@ -1,118 +1,53 @@
 function arenaAngles
-
+% Weird axis
+% X - backwards (minus goes right, plus goes left)
+% Y - is up and down
+% Z - far or close the object
+% It apply only for sphere so far as know (CHECK THIS OUT)
 global GL;
-
-eyesDistance = 0.04;
-stereoMode = 0; 
-stereoViews = 0;
 
 AssertOpenGL;
 Screen('Preference', 'SkipSyncTests', 1);
 InitializeMatlabOpenGL;
 PsychImaging('PrepareConfiguration');
 [win, winRect] = PsychImaging('OpenWindow', 0, 0, [], [], [],...
-    stereoMode, 0);
+    0, 0);
 
-% Set the keyboard
-KbName('UnifyKeyNames');
-leftKey = KbName('LeftArrow');
-rightKey = KbName('RightArrow');
-escapeKey = KbName('ESCAPE');
-
-% Hide cursor and prevent from writing into console
-HideCursor();
-ListenChar(2);
-
-% OpenGL part
 Screen('BeginOpenGL', win);
     ar = RectHeight(winRect) / RectWidth(winRect);
     glViewport(0, 0, RectWidth(winRect), RectHeight(winRect));
-    glColor3f(1,1,0);
+    glColor3f(1, 1, 0);
     glEnable(GL.LIGHT0);
     glMatrixMode(GL.PROJECTION);
     glLoadIdentity;
-    gluPerspective(45, 1/ar, 0.3, 100);
-    glMatrixMode(GL.MODELVIEW);
-    glLoadIdentity;
-    glLightfv(GL.LIGHT0,GL.POSITION,[ 1 2 3 0 ]);
+    gluPerspective(45, 1/ar, 0.03, 100);
+    glLightfv(GL.LIGHT0, GL.POSITION, [1 2 3 4]);
     glLightModelfv(GL.LIGHT_MODEL_TWO_SIDE, GL.TRUE);
-    glClearColor(0,0,0,0);   
+    glClearColor(0.0, 0.0, 0.0, 0.0);   
     glEnable(GL.LIGHTING);
     glEnable(GL.DEPTH_TEST);
 Screen('EndOpenGL', win);
 
-for epoch = 1:100
-    for view = 0:stereoViews
-        Screen('SelectStereoDrawbuffer', win, view);
-        Screen('BeginOpenGL', win);       
-            glMatrixMode(GL.MODELVIEW);
-            glLoadIdentity;
-            gluLookAt(1, 1, 1, 0, 0, 0, 0, 1, 0 );
-            glClear;   
-            
-            % Draw the whole arena here
-            glPushMatrix;
-                drawsphere(0.0, 0.0, -0.15, [1, 0, 0, 1]);
-            glPopMatrix;
-
-            glPushMatrix;
-                drawsphere(0.3, 0.0, -0.15, [1, 1, 1, 1]);
-            glPopMatrix;
-            
-            glPushMatrix;
-                drawsphere(0.0, 0.0, 0.0, [0, 1, 0, 1]);
-            glPopMatrix;
-            
-            glPushMatrix;
-                drawfloor();
-            glPopMatrix;
-
-            glPushMatrix;
-                drawarena();
-            glPopMatrix;
-
-            glPushMatrix;
-                drawmark();
-            glPopMatrix; 
-
-        Screen('EndOpenGL', win);
+Screen('BeginOpenGL', win);
+    glMatrixMode(GL.MODELVIEW);
+	glLoadIdentity;
+    %gluLookAt(-0.302, 0.350, 0.125, 0.462, 0.125, 0.0, 0, 1, 0);
+    gluLookAt(-0.302, 0.125, 0.350, 0.462, 0.0, 0.125, 0, 1, 0);
+    glClear;
+    glRotatef(90, 0, 1, 0);
+    try
+        drawsphere(-0.243, 0.014, 0.339, [1.0, 0.0, 0.0, 1.0]);
+        drawsphere(-0.111, 0.014, 0.070, [1.0, 1.0, 1.0, 1.0]);
+        drawmark(-0.017, 0.483);
+        drawfloor();
+        drawarena();
+    catch
+        psychlasterror;
+        sca;
     end
-
-    Screen('DrawingFinished', win, 2);
-    startTime = Screen('Flip', win);
-
-    while 1
-        [~, timeSecs, keyCode] = KbCheck;
-        if find(keyCode, 1) == leftKey
-            fprintf('Pressed: Left key, RT: %0.2f sec\n', timeSecs - startTime);
-            eyesDistance = eyesDistance - 0.01;
-            disp(eyePosition * eyesDistance)
-            KbReleaseWait;
-            break;
-        elseif find(keyCode, 1) == rightKey
-            fprintf('Pressed: Left key, RT: %0.2f sec\n', timeSecs - startTime);
-            eyesDistance = eyesDistance + 0.01;
-            disp(eyePosition * eyesDistance)
-            KbReleaseWait;
-            break;
-        elseif find(keyCode, 1) == escapeKey
-            ListenChar(0);
-            ShowCursor();
-            sca;
-            break;
-        end 
-    end
-    
-    % Blank screen
-    Screen('Flip', win);
-    WaitSecs(0.5);
-    
-end
-
-ListenChar(0);
-ShowCursor();
-Priority(0);
-KbQueueRelease;
+Screen('EndOpenGL', win);
+Screen('Flip', win);
+KbWait;
 sca;
 end
 
@@ -135,46 +70,57 @@ glTexImage2D(GL.TEXTURE_2D, 0, GL.RGBA, img_size(1), img_size(2),...
 end
 
 function drawsphere(x, y, z, color)
+size = 0.03;
 global GL
-glMaterialfv(GL.FRONT, GL.DIFFUSE, color);
-glTranslatef(x, y, z);
-qobj = gluNewQuadric();
-gluQuadricTexture(qobj, GL.TRUE);
-gluSphere(qobj, 0.03, 60, 60);
+glPushMatrix;
+    glTranslatef(x, y, z);
+    glMaterialfv(GL.FRONT, GL.DIFFUSE, color);
+    qobj = gluNewQuadric();
+    gluQuadricTexture(qobj, GL.TRUE);
+    gluSphere(qobj, size, 60, 60);
+glPopMatrix;
 end
 
 function drawarena()
 global GL
+height = 1.0;
+size = 0.5;
 readtexture('source/wall.jpg');
-glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
-qobj = gluNewQuadric();
-gluQuadricTexture(qobj, GL.TRUE);
-glRotatef(90, 1, 0, 0);
-glTranslatef(0, 0, -0.25);
-
-glEnable(GL.TEXTURE_2D);
-    gluCylinder(qobj, 0.5, 0.5, 0.25, 30, 30);
-glDisable(GL.TEXTURE_2D);
+glPushMatrix;
+    glRotatef(90, 1, 0, 0);
+    glTranslatef(0, 0, -height);
+    glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
+    qobj = gluNewQuadric();
+    gluQuadricTexture(qobj, GL.TRUE);
+    glEnable(GL.TEXTURE_2D);
+        gluCylinder(qobj, size, size, height, 60, 60);
+    glDisable(GL.TEXTURE_2D);
+glPopMatrix;
 end
 
 function drawfloor()
 global GL
 readtexture('source/floor.jpg');
-glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
-glRotatef(90, 1, 0, 0);
-qobj = gluNewQuadric();
-gluQuadricTexture(qobj, GL.TRUE);
-
-glEnable(GL.TEXTURE_2D);
-    gluDisk(qobj, 0, 0.5, 30, 30);
-glDisable(GL.TEXTURE_2D);
+glPushMatrix;
+    glRotatef(90, 1, 0, 0);
+    glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
+    qobj = gluNewQuadric();
+    gluQuadricTexture(qobj, GL.TRUE);
+    glEnable(GL.TEXTURE_2D);
+        gluDisk(qobj, 0, 0.5, 60, 60);
+    glDisable(GL.TEXTURE_2D);
+glPopMatrix;
 end
 
-function drawmark()
+function drawmark(x, y)
 global GL
-glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 0, 0]);
-qobj = gluNewQuadric();
-glTranslatef(0.0, 0.25, 0.48);
-glRotatef(90, 1, 0, 0);
-gluCylinder(qobj, 0.02, 0.02, 0.2, 30, 30);
+size = 0.2;
+diameter = 0.02;
+glPushMatrix;
+    glRotatef(90, 1, 0, 0);
+    glTranslatef(x, y, -size);
+    glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 0, 0]);
+    qobj = gluNewQuadric();
+    gluCylinder(qobj, diameter, diameter, size, 60, 60);
+glPopMatrix;
 end
