6a15406bc579ee0d517e237e89aef6ed4f9bf98c
neuropacabra <michael.tesar@nudz.cz>
2017-07-04 09:47:11 +0200

    Texture mapping on arena fixed
    This is a bit hack. Pay attention when coordinate system will be fixed too

--- arenaAngles.m
+++ arenaAngles.m
@@ -98,17 +98,18 @@ end
 
 function drawarena()
 global GL
-height = 1.0;
+height = 0.7;
 size = 0.5;
 readtexture('source/wall.jpg');
 glPushMatrix;
     glRotatef(90, 1, 0, 0);
+    glRotatef(90, 0, 0, 1);
     glTranslatef(0, 0, -height);
     glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 1, 0]);
     qobj = gluNewQuadric();
     gluQuadricTexture(qobj, GL.TRUE);
     glEnable(GL.TEXTURE_2D);
-        gluCylinder(qobj, size, size, height, 60, 60);
+        gluCylinder(qobj, size, size, height, 40, 40);
     glDisable(GL.TEXTURE_2D);
 glPopMatrix;
 end
