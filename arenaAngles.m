ae8316c89d3e95bfceb20096664a2a223f324708
neuropacabra <michael.tesar@nudz.cz>
2017-07-04 08:30:11 +0200

    Formatting the fucntion

--- arenaAngles.m
+++ arenaAngles.m
@@ -140,13 +140,10 @@ function viewPoint = computeview(angle)
 % Using sine law to compute AAS triangle
 % then add a lower part of circle x axe
 % to get the view point.
-a = 0.302; % camera offset
+a = 0.302;
 beta = 180 - angle;
 alpha = 180 - (90 + beta);
-
 b = a * sind(beta) / sind(alpha);
-
 offset = -0.350-(-0.5);
-
 viewPoint = b+offset;
 end
