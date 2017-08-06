43a0014420930554a601f8fbfcddf5f380b181d7
Michael Tesař <michael.tesar@nudz.cz>
2017-08-07 01:54:27 +0200

    Working debug in object programming aradigm

--- Graphic.m
+++ Graphic.m
@@ -2,84 +2,59 @@ classdef Graphic
     properties
         win;
         winRect;
+        textures;
     end
     
     methods
         function obj = Graphic
+            
             global GL;
             AssertOpenGL;
             screenid = max(Screen('Screens'));
             InitializeMatlabOpenGL(0, 0);
-            [obj.win , obj.winRect] = Screen('OpenWindow', screenid);
             HideCursor;
             ListenChar(2);
+            [obj.win, obj.winRect] = Screen('OpenWindow', screenid);
+            
+            floorImage = imread('source/floor.jpg');
+            obj.textures(1) = Screen('MakeTexture', obj.win, floorImage, [], 1);
+            wallImage = imread('source/wall.jpg');
+            obj.textures(2) = Screen('MakeTexture', obj.win, wallImage, [], 1);
             
             Screen('BeginOpenGL', obj.win);
-                ar = obj.winRect(4)/obj.winRect(3);
+                ar = obj.winRect(4) / obj.winRect(3);
                 glEnable(GL.LIGHTING);
                 glEnable(GL.LIGHT0);
                 glEnable(GL.DEPTH_TEST);
-                glLightModelfv(GL.LIGHT_MODEL_TWO_SIDE,GL.TRUE);
-                
-                glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ .33 .22 .03 1 ]);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ .78 .57 .11 1 ]);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.SHININESS,27.8);
-
                 glMatrixMode(GL.PROJECTION);
                 glLoadIdentity;
-                gluPerspective(25,1/ar,0.1,100);
-
+                gluPerspective(25, 1/ar, 0.1, 100);
                 glMatrixMode(GL.MODELVIEW);
                 glLoadIdentity;
-                gluLookAt(0,0,5,0,0,0,0,1,0);
-                glClearColor(0,0,0,0);
-
-                glLightfv(GL.LIGHT0,GL.POSITION,[ 1 2 3 0 ]);
-                glLightfv(GL.LIGHT0,GL.DIFFUSE, [ 1 1 1 1 ]);
-                glLightfv(GL.LIGHT0,GL.AMBIENT, [ .1 .1 .1 1 ]);
+                gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0, 1, 0);
+                glClearColor(0.0, 0.0, 0.0, 0.0);
+                glLightfv(GL.LIGHT0,GL.POSITION,[1.0, 2.0, 3.0, 0.0]);
+                glLightfv(GL.LIGHT0,GL.DIFFUSE, [1.0, 1.0, 1.0, 1.0]);
+                glLightfv(GL.LIGHT0,GL.AMBIENT, [0.1, 0.1, 0.1, 1.0]);
                 glClear;
             Screen('EndOpenGL', obj.win);
         end
              
-        function drawText(obj, text)
+        function debug(obj, text)
              DrawFormattedText(obj.win, text,...
-                 'center', 'center', [255 0 0]);
+                 100, 100, [255 255 0]);
         end
         
-        function setView(obj, eyeX, eyeY, eyeZ, pointX, pointY, pointZ)
+        function setView(obj, camera, target)
             global GL;
             Screen('BeginOpenGL', obj.win);
                 glMatrixMode(GL.MODELVIEW);
                 glLoadIdentity;
-                gluLookAt(eyeX,eyeY,eyeZ,pointX,pointY,pointZ,0,1,0);
-                glClear;
+                gluLookAt(camera(1), camera(2), camera(3),...
+                    target(1), target(2), target(3),...
+                    0, 1, 0);
+                glClear;               
             Screen('EndOpenGL', obj.win);
-        end
-        
-        function drawPlanet(obj, texture, x, y, z)
-            global GL;
-            myimg = imread(texture);
-            mytex = Screen('MakeTexture', obj.win, myimg, [], 1);
-            [gltex, gltextarget] = Screen('GetOpenGLTexture', obj.win, mytex);
-            Screen('BeginOpenGL', obj.win);
-                glEnable(gltextarget);
-                glBindTexture(gltextarget, gltex);
-                glTexEnvfv(GL.TEXTURE_ENV,GL.TEXTURE_ENV_MODE,GL.MODULATE);
-                glTexParameteri(gltextarget, GL.TEXTURE_WRAP_S, GL.REPEAT);
-                glTexParameteri(gltextarget, GL.TEXTURE_WRAP_T, GL.REPEAT);
-                glTexParameteri(gltextarget, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
-                glTexParameteri(gltextarget, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ 1 1 1 1 ]);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ 1 1 1 1 ]);
-        
-                mysphere = gluNewQuadric;
-                gluQuadricTexture(mysphere, GL.TRUE); 
-                glPushMatrix;
-                    glTranslatef(x, y, z);
-                    glRotatef(40,1,0,0);
-                    gluSphere(mysphere, 0.4, 60, 60);
-                glPopMatrix;
-            Screen('EndOpenGL', obj.win);    
         end
         
         function drawInstruction(obj, text)
@@ -89,95 +64,83 @@ classdef Graphic
                  'center', 'center', [255 255 255]); 
         end
         
-        function drawArena(obj, rx, ry, rz, wx, wy, wz)
+        function drawArena(obj)
             global GL;
             
-            % Red sphere
+            % Mark
             Screen('BeginOpenGL', obj.win);
-            glPushMatrix;
-                glMaterialfv(GL.FRONT, GL.DIFFUSE, [1 0 0]);
-                glTranslatef(rx, ry, rz);
-                qobj = gluNewQuadric();
-                gluQuadricTexture(qobj, GL.TRUE);
-                gluSphere(qobj, 0.03, 60, 60);
-            glPopMatrix;
-            Screen('EndOpenGL', obj.win);
-
-            % White sphere
-            Screen('BeginOpenGL', obj.win);
-            glPushMatrix;
-                glMaterialfv(GL.FRONT, GL.DIFFUSE, [1 1 1]);
-                glTranslatef(wx, wy, wz);
-                qobj = gluNewQuadric();
-                gluQuadricTexture(qobj, GL.TRUE);
-                gluSphere(qobj, 0.03, 60, 60);
-            glPopMatrix;
-            Screen('EndOpenGL', obj.win);
-
-            % Walls
-            wallImage = imread('source/wall.jpg');
-            wallTex = Screen('MakeTexture', obj.win, wallImage, [], 1);
-            [wallgltex, wallgltextarget] = Screen('GetOpenGLTexture', obj.win, wallTex);
-            Screen('BeginOpenGL', obj.win);
-                glEnable(wallgltextarget);
-                glBindTexture(wallgltextarget, wallgltex);
-                glTexEnvfv(GL.TEXTURE_ENV,GL.TEXTURE_ENV_MODE,GL.MODULATE);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_S, GL.REPEAT);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_T, GL.REPEAT);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ 1 1 1 1 ]);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ 1 1 1 1 ]);
-
-                qobj = gluNewQuadric;
-                gluQuadricTexture(qobj, GL.TRUE); 
-                glPushMatrix;
-                    glTranslatef(0, 0, 0);
-                    glRotatef(40,1,0,0);
-                    gluCylinder(qobj, 0.5, 0.5, 30, 30, 30);
-                glPopMatrix;
-            Screen('EndOpenGL', obj.win);
-
-            % Floor
-            wallImage = imread('source/floor.jpg');
-            wallTex = Screen('MakeTexture', obj.win, wallImage, [], 1);
-            [wallgltex, wallgltextarget] = Screen('GetOpenGLTexture', obj.win, wallTex);
-            Screen('BeginOpenGL', obj.win);
-                glEnable(wallgltextarget);
-                glBindTexture(wallgltextarget, wallgltex);
-                glTexEnvfv(GL.TEXTURE_ENV,GL.TEXTURE_ENV_MODE,GL.MODULATE);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_S, GL.REPEAT);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_T, GL.REPEAT);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
-                glTexParameteri(wallgltextarget, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ 1 1 1 1 ]);
-                glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ 1 1 1 1 ]);
-
-                qobj = gluNewQuadric;
-                gluQuadricTexture(qobj, GL.TRUE); 
-                glPushMatrix;
-                    glTranslatef(0, 0, 0);
-                    glRotatef(-80,1,0,0);
-                    gluDisk(qobj, 0, 5, 50, 50);
-                glPopMatrix;
-            Screen('EndOpenGL', obj.win);   
-            
-            % Marks
-            Screen('BeginOpenGL', obj.win);
+            markSize = 0.05;
+            markDiameter = 0.02;
             glMaterialfv(GL.FRONT, GL.DIFFUSE, [1, 1, 0, 0]);
             qobj = gluNewQuadric();
-            glTranslatef(0.0, 0.0, 0.5);
-            glRotatef(90, 1, 0, 0);
             glPushMatrix;
-                gluCylinder(qobj, 0.07, 0.07, 0.2, 30, 30);
+                glTranslatef(0.017, markSize, -0.483);
+                glRotatef(90, 1, 0, 0);
+                gluCylinder(qobj, markDiameter, markDiameter, markSize, 30, 30);
+            glPopMatrix;
+            Screen('EndOpenGL', obj.win);
+            
+            % Floor
+            floorSize = 0.5;
+            [wallgltex, wallgltextarget] = Screen('GetOpenGLTexture', obj.win, obj.textures(1));
+            Screen('BeginOpenGL', obj.win);
+            glEnable(wallgltextarget);
+            glBindTexture(wallgltextarget, wallgltex);
+            glTexEnvfv(GL.TEXTURE_ENV,GL.TEXTURE_ENV_MODE,GL.MODULATE);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_S, GL.REPEAT);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_T, GL.REPEAT);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+            glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ 1 1 1 1 ]);
+            glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ 1 1 1 1 ]);
+            qobj = gluNewQuadric;
+            gluQuadricTexture(qobj, GL.TRUE); 
+            glPushMatrix;
+                glTranslatef(0, 0, 0);
+                glRotatef(90, 1, 0, 0);
+                gluDisk(qobj, 0, floorSize, 50, 50);
+            glPopMatrix;
+            Screen('EndOpenGL', obj.win);
+            
+            % Walls
+            wallHeight = 0.1;
+            wallSize = 0.5;
+            [wallgltex, wallgltextarget] = Screen('GetOpenGLTexture', obj.win, obj.textures(2));
+            Screen('BeginOpenGL', obj.win);
+            glEnable(wallgltextarget);
+            glBindTexture(wallgltextarget, wallgltex);
+            glTexEnvfv(GL.TEXTURE_ENV,GL.TEXTURE_ENV_MODE,GL.MODULATE);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_S, GL.REPEAT);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_WRAP_T, GL.REPEAT);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+            glTexParameteri(wallgltextarget, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+            glMaterialfv(GL.FRONT_AND_BACK,GL.AMBIENT, [ 1 1 1 1 ]);
+            glMaterialfv(GL.FRONT_AND_BACK,GL.DIFFUSE, [ 1 1 1 1 ]);
+            qobj = gluNewQuadric;
+            gluQuadricTexture(qobj, GL.TRUE); 
+            glPushMatrix;
+                glRotatef(90, 1, 0, 0);
+                glTranslatef(0, 0, -wallHeight);
+                gluCylinder(qobj, wallSize, wallSize, wallHeight, 30, 30);
             glPopMatrix;
             Screen('EndOpenGL', obj.win);
         end
         
         function wait(obj)
             Screen('Flip', obj.win);
-            KbWait;
-            KbReleaseWait;
+            %KbWait(-1);
+            %KbReleaseWait;
+        end
+        
+        function sphere(obj, coord)
+            global GL;
+            Screen('BeginOpenGL', obj.win);
+            glPushMatrix;
+                glTranslatef(coord(1), coord(2), coord(3));
+                glMaterialfv(GL.FRONT, GL.DIFFUSE, [0, 0, 1]);
+                glutSolidSphere(0.02, 30, 30);
+            glPopMatrix;
+            Screen('EndOpenGL', obj.win);
         end
         
         function close(obj)
@@ -186,7 +149,7 @@ classdef Graphic
             ListenChar;
             sca;
             fprintf('Done!\n');
-        end
+        end 
     end
 end
             
--- arena.m
+++ arena.m
@@ -1,7 +1,68 @@
-g = Graphic;
+camera = [0.0, 0.0, 5.0];
+target = [0.0, 0.0, 0.0];
 
-g.setView(0, 0, 4, 0, 0, 0);
-g.drawArena(-0.5, 0, 0, 0.5, 0, 0);
-g.wait;
+red = [0.234, 0.339, 0.014];
+white = [0.111, 0.70, -0.014];
 
-g.close;
+try
+    g = Graphic;
+catch
+    warning('Cannot initialize scene!');
+    psychrethrow(psychlasterror);
+end
+
+while true
+    try
+        g.setView(camera, target);
+        g.drawArena;
+
+        text = sprintf('Camera:\nX = %0.2f, Y = %0.2f, Z = %0.2f',...
+            camera(1), camera(2), camera(3));
+        text = strcat(text,...
+            sprintf('\nLook at:\nX = %0.2f, Y = %0.2f, Z = %0.2f\n\n',...
+            target(1), target(2), target(3)));
+        g.debug(text);
+ 
+        g.sphere([0.0, 0.0, 0.0]);
+
+        g.wait;
+    catch
+        g.close;
+        warning('Cannot draw arena models!');
+        psychrethrow(psychlasterror);
+        break;
+    end
+    
+    [t, key, ~] = KbWait;
+    if find(key, 1) == KbName('LeftArrow')
+        camera(1) = camera(1) - 0.1;
+    elseif find(key, 1) == KbName('RightArrow');
+        camera(1) = camera(1) + 0.1;
+    elseif find(key, 1) == KbName('UpArrow');
+        camera(2) = camera(2) + 0.1;
+    elseif find(key, 1)  == KbName('DownArrow');
+        camera(2) = camera(2) - 0.1;
+    elseif find(key, 1) == KbName('w');
+        camera(3) = camera(3) - 0.1;
+    elseif find(key, 1) == KbName('s');
+        camera(3) = camera(3) + 0.1;
+    elseif find(key, 1) == KbName('r');
+        camera = [0.0, 0.0, 10.0]; 
+        target = [0.0, 0.0, 0.0];
+    elseif find(key, 1) == KbName('a');
+        target(1) = target(1) - 0.1; 
+    elseif find(key, 1) == KbName('d');
+        target(1) = target(1) + 0.1;
+    elseif find(key, 1) == KbName('q');
+        target(2) = target(2) - 0.1; 
+    elseif find(key, 1) == KbName('e');
+        target(2) = target(2) + 0.1;
+    elseif find(key, 1) == KbName('x');
+        target(3) = target(3) - 0.1; 
+    elseif find(key, 1) == KbName('c');
+        target(3) = target(3) + 0.1;
+    elseif find(key, 1) == KbName('ESCAPE');
+        g.close;
+        break;
+    end 
+end
