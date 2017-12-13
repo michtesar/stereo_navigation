33a0d8b53135f2a58b92e5ca246761d9425da76d
Michael Tesař <michael.tesar@nudz.cz>
2017-12-13 12:55:34 +0100

    Fix fixation duration

--- Stereo.m
+++ Stereo.m
@@ -122,11 +122,6 @@ for trial = 1:height(source)
         
     end
     
-    % Draw fixation cross for t = 1500 ms
-    DrawFormattedText(win, '+', 'center', 'center', [1 1 1]);
-    Screen('Flip', win);
-    WaitSecs(1.5);
-    
     for view = 0:1
         Screen('SelectStereoDrawbuffer', win, view);
         Screen('BeginOpenGL', win);
@@ -212,10 +207,10 @@ for trial = 1:height(source)
         warning('Cannot send response tag');
     end
     
-    % Show fixation cross for t = 500 ms
+    % Show fixation cross for t = 1500 ms
     DrawFormattedText(win, '+', 'center', 'center', [1 1 1]);
     Screen('Flip', win);
-    WaitSecs(0.5);
+    WaitSecs(1.5);
     
     % If defined show feedback of single trial for t = 500 ms
     if source.Feedback(trial)
